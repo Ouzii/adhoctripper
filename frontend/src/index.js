0bf9381325106fad19a89879ccf8a1b13aa9eec5
Ouzii <oskari.laaja@gmail.com>
2019-03-25 11:29:23 +0200

    Lisätty iconit ja muokattu manifestia. Alettu PWA-toiminnallisuuksien implementointi. Tuntikirjanpito päivitetty.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,10 +15,9 @@ const render = () => {
         </Provider>
         , document.getElementById('root'));
 }
-
+serviceWorker.register();
 render()
 store.subscribe(render)
-serviceWorker.unregister();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
